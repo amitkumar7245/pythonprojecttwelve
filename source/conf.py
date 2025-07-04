133d3bed8134ed61fc057db61bef4c5213573f9d
amitkumar7245 <amitkdna@gmail.com>
2025-07-05 01:44:06 +0530

    content upload full

--- source/conf.py
+++ source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'PythonProjectTWeLIVE'
+project = 'Coinbasehelpcenter'
 copyright = '2025, Author'
 author = 'Author'
 

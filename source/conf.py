6b633004fa1f9102ad135a18eb631ab8c58ef3a1
amitkumar7245 <amitkdna@gmail.com>
2025-07-05 01:29:15 +0530

    content full update

--- source/conf.py
+++ source/conf.py
@@ -27,14 +27,4 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 
-# -- Meta Tags Injection: Bing & Canonical -----------------------------------
 
-def add_custom_meta_tags(app, pagename, templatename, context, doctree):
-    context['metatags'] = context.get('metatags', '') + (
-        # '<meta name="msvalidate.01" content="0CD364F244E1C3C66ED4002465FC0441" />\n'
-        # '<link rel="canonical" href="https://coinbase-support.readthedocs.io" />\n'
-    )
-
-def setup(app):
-    app.add_config_value('meta_tags', '', 'html')
-    app.connect('html-page-context', add_custom_meta_tags)
